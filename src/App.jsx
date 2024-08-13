56ba46aabbfef045fc1feb51dd0f83b19f21eacc
Silvestre Fernandez <rasa356@outlook.com>
2024-08-13 15:30:46 +1200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import './App.css'
+const cityInput = document.getElementById('city-input')
+const displayWeatherInfo = document.getElementById('condition')
+const errorMessage = document.getElementById('text-output')
+import.meta.env.WEATHER_API_KEY
+// import { useState } from 'react';
 
 function App() {
- 
 
+// const [city, changeCity] = useState({currentCity})
+// 
+
+
+function handleClick() {
+const getWeather = fetch(
+  `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.WEATHER_API_KEY}&q=${cityInput || "Tokyo" }&aqi=no`
+ )
+  .then((res) => res.json())
+  .then((data) => {
+    displayWeatherInfo.innerHTML =
+    `<p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
+     <p>It's ${data.current.condition.text}.</p>
+     <img src="${data.current.condition.icon}" alt="icon"/>
+     `;
+  })
+  .catch((err) => {
+    console.log(err);
+    errorMessage.innerHTML = `Please enter a valid city.`
+  })
+ }
+  
   return (
     <>
      <div className="main-body">
@@ -13,13 +39,18 @@ function App() {
                  <br />
                  <input type='text' id='city-input' placeholder='e.g. Tokyo'></input>
                  <br />
-                 <button type='submit' id="submit-button">Submit</button>
+                 <button type='submit' id="submit-button" onClick={handleClick}>Search</button>
                </form>
-               <div className='text-output'></div>
+               <div className='text-output' id='text-output'>
+                <h2>Your weather information:</h2>
+                <p id="condition"></p>
+                <span id="icon"></span>
+               </div>
             </div>
             
         </div>
     </>
+
   )
 }
 
